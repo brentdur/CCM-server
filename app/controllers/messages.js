7661586e4e1e5c34b19b805a3461a7be1a8368fe
Brenton Durkee <brentdur@gmail.com>
2015-08-01 16:45:34 -0400

    Fix messages post headers error

--- app/controllers/messages.js
+++ app/controllers/messages.js
@@ -87,6 +87,7 @@ router.post('/', auth.canWrite('Msgs'), function(req, res, next){
     }
 
     ], function(err){
+      console.log(err);
       if(err) return next(err);
       console.log('done');
       res.sendStatus(200);
