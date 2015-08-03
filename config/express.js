c3ffe65c76aa3f0999a928882e7c4e5324c06288
Brenton Durkee <brentdur@gmail.com>
2015-08-02 22:08:44 -0400

    Erased unneeded lines

--- config/express.js
+++ config/express.js
@@ -46,8 +46,7 @@ module.exports = function(app, config) {
   });
 
   app.use('/api/docs', express.static(config.root + '/docs'));
-  // app.use('/download', express.static(config.root + '/public/app-release.apk'));
-  // 
+
   app.get('/download', function(req, res){
     res.sendFile(config.root + '/public/app-release.apk');
   });
