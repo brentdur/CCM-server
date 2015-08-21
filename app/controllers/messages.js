efc4aff49d10f33f90097333e00057bd64ddc2f9
Brenton Durkee <brentdur@users.noreply.github.com>
2015-08-20 22:13:39 -0400

    Update messages.js

--- app/controllers/messages.js
+++ app/controllers/messages.js
@@ -191,7 +191,8 @@ router.post('/', auth.canWrite('Msgs'), function(req, res, next){
  */
 
 router.delete('/', auth.inGroup('ministers'), function(req, res,next){
-  var message = req.body.message.toString();
+  var message = req.body.message;
+  console.log(message);
   Message.findById(message).remove(function(err, data){
     if(err) return next(err);
     console.log(data.result);
