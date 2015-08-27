e81787f239ff02e25fd111b154cf9120b6c3220d
Brenton Durkee <brentdur@gmail.com>
2015-08-26 20:00:37 -0400

    Update to homepage

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -15,6 +15,7 @@ var express = require('express'),
   Signup = mongoose.model('Signup'),
   async = require('async');
   var gcm = require('../gcm');
+  var config = require('../../config/config');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -22,60 +23,67 @@ module.exports = function (app) {
 
 //gets all the different information and assigns the arrays to variables delivered to the webpage
 router.get('/', function (req, res, next) {
-  async.series([
-      function(callback){
-        Event.find(function (err, events) {
-          callback(err, events);
-       });
-      },
-      function(callback){
-        Message.find(function (err, messages){
-          callback(err, messages);
-        });
-      },
-      function(callback){
-        Talk.find(function (err, talks){
-          callback(err, talks);
-        });
-      },
-      function(callback){
-        Location.find(function(err, locations){
-          callback(err, locations);
-        });
-      },
-      function(callback){
-        User.find(function(err, users){
-          callback(err, users);
-        });
-      },
-      function(callback){
-        Group.find(function(err, groups){
-          callback(err, groups);
-        });
-      },
-      function(callback){
-        Topic.find(function(err, topics){
-          callback(err, topics);
-        });
-      },
-      function(callback){
-        Signup.find(function(err, signups){
-          callback(err, signups);
-        });
-      }
-    ], function(err, results){
-      if (err) return next(err);
-      res.render('index', {
-          title: 'Generator-Express MVC',
-          events: results[0],
-          msgs: results[1],
-          talks: results[2],
-          locs: results[3],
-          users: results[4],
-          groups: results[5],
-          topics: results[6],
-          signups: results[7]
+  if(config.env !== 'production'){
+    async.series([
+        function(callback){
+          Event.find(function (err, events) {
+            callback(err, events);
+         });
+        },
+        function(callback){
+          Message.find(function (err, messages){
+            callback(err, messages);
           });
+        },
+        function(callback){
+          Talk.find(function (err, talks){
+            callback(err, talks);
+          });
+        },
+        function(callback){
+          Location.find(function(err, locations){
+            callback(err, locations);
+          });
+        },
+        function(callback){
+          User.find(function(err, users){
+            callback(err, users);
+          });
+        },
+        function(callback){
+          Group.find(function(err, groups){
+            callback(err, groups);
+          });
+        },
+        function(callback){
+          Topic.find(function(err, topics){
+            callback(err, topics);
+          });
+        },
+        function(callback){
+          Signup.find(function(err, signups){
+            callback(err, signups);
+          });
+        }
+      ], function(err, results){
+        if (err) return next(err);
+        res.render('index', {
+            title: 'CCM Server',
+            events: results[0],
+            msgs: results[1],
+            talks: results[2],
+            locs: results[3],
+            users: results[4],
+            groups: results[5],
+            topics: results[6],
+            signups: results[7]
+            });
 
-  });
+    });
+  }
+  else {
+    res.render('partials/welcome', {
+            title: 'CCM'
+            });
+  }
 });
